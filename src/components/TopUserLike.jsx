16d3dff092b864a06b9deefef9a200f2953e0881
habibouomar <habibou.omar@gmail.com>
2022-05-19 12:29:13 +0200

    wip frontend

--- src/components/TopUserLike.jsx
+++ src/components/TopUserLike.jsx
@@ -16,7 +16,7 @@ function TopUserLike() {
                         <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
 
                             <div className="ms-2 me-auto">
-                                <div className="fw-bold">Marc</div>
+                                <div className="fw-bold">Marie</div>
                                <p>The world is mine</p> 
                             </div>
 
--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 import "../styles/profil.css"
-import { GiPositionMarker } from 'react-icons/gi';
-
-
 
 function Profil() {
+
     const [state, setState] = useState({
         name: "",
         bio: ""
     });
+
     const [name, setName] = useState('Omar')
     const [newName, setNewName] = useState('')
 
-    const [bio, setBio] = useState('')
+    const [bio, setBio] = useState('The world is mine')
     const [newBio, setNewBio] = useState('')
+    
     function handleChange(evt) {
         const value = evt.target.value;
         setNewName(value)
@@ -32,7 +32,6 @@ function Profil() {
         setBio(newBio)
     }
 
-
     return (
         <body>
 
@@ -40,15 +39,13 @@ function Profil() {
                 <div className="pic">
                     <img src="https://picsum.photos/130/130?image=1027" className="rounded-circle float-start" alt="..." />
                     <button type="button" className="btn btn-dark rounded-pill border-white" data-bs-toggle="modal" data-bs-target="#exampleModal">Modifier profil</button>
-
-
-
                 </div>
             </div>
+
             <div className="user">
                 <h2 className="username">{name}</h2>
-
             </div>
+
             <div className="bio">
                 <p>{bio}</p>
             </div>
@@ -59,6 +56,7 @@ function Profil() {
                         <div className="modal-header">
 
                             <h5 className="modal-title" id="exampleModalLabel">Modifier le profil</h5>
+
                             <div class="mb-3">
                                 <label for="recipient-name" class="col-form-label">Username</label>
                                 <input class="form-control"
@@ -68,6 +66,7 @@ function Profil() {
                                     name="name"
                                     maxLength="10" />
                             </div>
+
                             <div class="mb-3">
                                 <label for="recipient-name" class="col-form-label">Bio</label>
                                 <input class="form-control"
@@ -77,12 +76,14 @@ function Profil() {
                                     maxLength="32"
                                 />
                             </div>
+
                         </div>
 
                         <div className="modal-footer">
                             <button type="button" className="btn btn-white border-dark" data-bs-dismiss="modal">Fermer</button>
                             <button type="button" className="btn btn-dark" onClick={modalSubmit}>Enregistrer</button>
                         </div>
+                        
                     </div>
                 </div>
             </div>
