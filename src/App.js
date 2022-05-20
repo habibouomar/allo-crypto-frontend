736d43bd9dddee3a1346ee6d4f65353571e67820
IBEPP <ibrahimajahitee@gmail.com>
2022-05-20 16:33:49 +0200

    login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './styles/App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Router } from './routes/Router';
+import "./styles/App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Router } from "./routes/Router";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Router/>
+      <Router />
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../style/login.css";
+
+const Login = () => {
+  const [user, setUser] = useState("");
+  const [bio, setBio] = useState("");
+  const [img, setImg] = useState("");
+
+  const poster = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3002/user", {
+      method: "POST",
+      headers: new Headers({ "content-type": "application/json" }),
+      body: JSON.stringify({
+        userName: user,
+        aboutMe: bio,
+        picture: img,
+      }),
+    })
+      .then((res) => res.json(res))
+      .then((json) => console.log(json));
+  };
+
+  const getData = (e) => {
+    e.preventDefault();
+    setUser(e.target.value);
+  };
+
+  const getBio = (e) => {
+    e.preventDefault();
+    setBio(e.target.value);
+  };
+
+  const getPicture = (e) => {
+    e.preventDefault();
+    setImg(e.target.value);
+  };
+
+  return (
+    <div className="div-form">
+      <form>
+        <div>
+          <input type="text" name="name" onChange={getData} />
+        </div>
+        <div>
+          <textarea name="bio" onChange={getBio} />
+        </div>
+        <div>
+          <input className="ii" type="text" name="img" onChange={getPicture} />
+        </div>
+        <div>
+          <button type="submit" onClick={poster}>
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
