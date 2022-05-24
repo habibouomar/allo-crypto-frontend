51f2428917b560499c5d8ceb17f7bffdea484069
habibouomar <habibou.omar@gmail.com>
2022-05-24 15:33:16 +0200

    ft-topuser

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./styles/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Router } from "./routes/Router";
-import Header from "./components/Header";
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Header></Header> */}
       <Router/>
     </div>
   );
